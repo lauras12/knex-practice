4ac22d9280e5d610f737124f8490b8a767a23292
lauras12 <56985938+lauras12@users.noreply.github.com>
2020-09-09 13:19:47 -0400

    Delete setup.js

--- setup.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test'
-process.env.API_TOKEN = 'test-auth-token'
-
-require('dotenv').config()
-const { expect } = require('chai')
-const supertest = require('supertest')
-
-global.expect = expect
-global.supertest = supertest
